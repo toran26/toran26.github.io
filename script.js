591658144cc3766704df24042018bb6ab0721f58
Ujjwal Poudel <ujj1119wal@gmail.com>
2019-05-09 22:16:32 +0545

    hotel
    new hotel

---
+++ script.js
@@ -0,0 +1,13 @@
+// Navigation
+	function openNav() {
+	  document.getElementById("mySidenav").style.width = "100%";
+	  document.getElementById("main").style.marginLeft = "250px";
+	}
+
+	function closeNav() {
+	  document.getElementById("mySidenav").style.width = "0";
+	  document.getElementById("main").style.marginLeft= "0";
+	}
+// 
+
+
